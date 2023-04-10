aef161a6887d88957e379287abe0cfd9a68edb72
Ilya <shulapov1999@gmail.com>
2023-04-10 13:11:02 +0300

    add project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {About} from "./Components/About/About";
+import {Header} from "./Components/Header/Header";
+import {Skills} from "./Components/Skills/Skills";
+import {Projects} from "./Components/Projects/Projects";
+import {HireMe} from "./Components/HireMe/HireMe";
+import {Contacts} from "./Components/Contacts/Contacts";
+import {Footer} from "./Components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <About/>
+            <Skills/>
+            <Projects/>
+            <HireMe/>
+            <Contacts/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
