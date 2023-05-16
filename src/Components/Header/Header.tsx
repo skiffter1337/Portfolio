57de15ce01eb3221f5ef0079e65a3ea5d112a0fa
Ilya <shulapov1999@gmail.com>
2023-05-16 14:44:50 +0300

    start creating burger menu in Header.tsx

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,40 +1,67 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Header.module.css'
 import logo from '../../assets/image/logoIS.png'
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Header = () => {
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    let headerStyles = scrolled ? `${s.header} ${s.header_scroll}` : `${s.header}`
+
     return (
-        <div className={s.header}>
+        <div className={headerStyles}>
             <div className={s.header_container}>
-            <div>
-                <a>
-                    <img src={logo}/>
-                </a>
-            </div>
-            <nav className={s.nav_container}>
                 <div>
-                    <ul className={s.nav_list}>
-                        <li>
-                            <a href="">Home</a>
-                        </li>
-                        <li>
-                            <a href="">Main</a>
-                        </li>
-                        <li>
-                            <a href="">About me</a>
-                        </li>
-                        <li>
-                            <a href="">Skills</a>
-                        </li>
-                        <li>
-                            <a href="">Projects</a>
-                        </li>
-                        <li>
-                            <a href="">Contacts</a>
-                        </li>
-                    </ul>
+                    <a>
+                        <img src={logo}/>
+                    </a>
                 </div>
-            </nav>
+                <div className={s.burger_wrapper}>
+                    <div className={s.burger_icon}>
+                      <MenuIcon fontSize={"large"}/>
+                    </div>
+                </div>
+                <nav className={s.nav_container}>
+                    <div>
+                        <ul className={s.nav_list}>
+                            <li>
+                                <a href="">Home</a>
+                            </li>
+                            <li>
+                                <a href="">Main</a>
+                            </li>
+                            <li>
+                                <a href="">About me</a>
+                            </li>
+                            <li>
+                                <a href="">Skills</a>
+                            </li>
+                            <li>
+                                <a href="">Projects</a>
+                            </li>
+                            <li>
+                                <a href="">Contacts</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
     );
