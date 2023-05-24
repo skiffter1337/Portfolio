50604a8e614f6fd85ce9ce64cb9fdb9a4a128006
Ilya <shulapov1999@gmail.com>
2023-05-24 19:33:36 +0300

    refactor Socials.tsx

--- src/Components/Footer/Socials/Socials.tsx
+++ src/Components/Footer/Socials/Socials.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import s from './Socials.module.css'
-import telegram from './../../../assets/image/telegram.svg'
-import facebook from './../../../assets/image/facebook.svg'
-import linkedin from './../../../assets/image/in.svg'
-import whatsapp from './../../../assets/image/whatsapp.svg'
+import s from './Socials.module.css';
+import { ReactComponent as TelegramIcon } from './../../../assets/socials/telegram-plane.svg';
+import { ReactComponent as FacebookIcon } from './../../../assets/socials/facebook-f.svg';
+import { ReactComponent as LinkedinIcon } from './../../../assets/socials/linkedin-in.svg';
+import { ReactComponent as WhatsappIcon } from './../../../assets/socials/whatsapp.svg';
 
 export const Socials = () => {
     return (
         <div className={s.socialsLinks}>
-            <div><a><img src={telegram}/></a></div>
-            <div><a><img src={facebook}/></a></div>
-            <div><a><img src={linkedin}/></a></div>
-            <div><a><img src={whatsapp}/></a></div>
+            <div>
+                <a href="#">
+                    <TelegramIcon className={s.telegramIcon} />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <FacebookIcon className={s.fbIcon} />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <LinkedinIcon className={s.linkedinIcon} />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <WhatsappIcon className={s.whatsappIcon} />
+                </a>
+            </div>
         </div>
     );
 };
-
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -2,33 +2,51 @@ import React from 'react';
 import s from './Main.module.scss'
 import KEKS_MAIN from './../../assets/image/keks_main.jpg'
 import {Button} from "../../common/components/Button/Button";
+import MAIN1 from '../../assets/particles/main-element-1.png'
+import MAIN2 from '../../assets/particles/main-element-2.png'
+import MAIN3 from '../../assets/particles/main-element-3.png'
+import MAIN4 from '../../assets/particles/main-element-4.png'
+import MAIN5 from '../../assets/particles/main-element-5.png'
+import {Socials} from "../Footer/Socials/Socials";
 
 
 export const Main = () => {
     return (
         <section className={s.main_block}>
             <div className={s.main_container}>
-            <div className={s.greeting}>
-                <h1>
-                    <div className={s.small}>Hello, I'm</div>
-                    Ilya
-                    <div className={s.strong}>Shulapov</div>
-                </h1>
+                <div className={s.greeting}>
+                    <h1>
+                        <div className={s.small}>Hello, I'm</div>
+                        Ilya
+                        <div className={s.strong}>Shulapov</div>
+                    </h1>
 
-                <p>I develop websites using...</p>
-                <div>
-                    <p>
-                        {"             "}
-                    </p>
+                    <p>I develop websites using...</p>
+                    <div>
+                        <p>
+                            {"             "}
+                        </p>
+                    </div>
+                    <div className={s.button_container}>
+                        <div>
+                            <Button title={"contact me"}/>
+                            </div>
+                            <div>
+                                <Socials/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={s.photo_block}>
+                        <img src={KEKS_MAIN} alt={"Ilya Shulapov"} className={s.photo}/>
+                    </div>
                 </div>
-                <div className={s.button_container}>
-                    <Button title={"contact me"}/>
+                <div className={s.design_element}>
+                    <img className={`${s.de_main1} ${s.elem_updown}`} src={MAIN1}/>
+                    <img className={`${s.de_main2} ${s.elem_move}`} src={MAIN2}/>
+                    <img className={`${s.de_main3} ${s.elem_updown}`} src={MAIN3}/>
+                    <img className={`${s.de_main4} ${s.elem_updown}`} src={MAIN4}/>
+                    <img className={`${s.de_main5} ${s.elem_move}`} src={MAIN5}/>
                 </div>
-            </div>
-            <div className={s.photo_block}>
-                <img src={KEKS_MAIN} alt={"main photo"} className={s.photo}/>
-            </div>
-            </div>
         </section>
-    );
+);
 };
